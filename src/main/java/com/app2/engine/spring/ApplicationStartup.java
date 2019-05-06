de7d8269ea55f60f407f4911a3cf91fe2567b753
thongchai <thongchai.sh@gmail.com>
2019-05-06 10:45:10 +0700

    update

---
+++ src/main/java/com/app2/engine/spring/ApplicationStartup.java
@@ -0,0 +1,29 @@
+package com.app2.engine.spring;
+
+import com.app2.engine.repository.AppParameterRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
+    private static final Logger LOGGER = LogManager.getLogger(ApplicationStartup.class);
+
+    @Autowired
+    private AppParameterRepository appParameterRepository;
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        LOGGER.info("ApplicationStartup.....!");
+        LOGGER.info("Swagger UI : /swagger-ui.html");
+        LOGGER.info("Spring Data REST : /rest-api");
+
+//        AppParameter appParameterConfig = appParameterRepository.findByCode("50");//example
+    }
+
+}
