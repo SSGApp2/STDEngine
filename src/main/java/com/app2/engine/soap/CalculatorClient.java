36fab87fe18c708b4e3eecc557841019f1927be9
thongchai <thongchai_s@softsquaregroup.com>
2020-07-17 14:13:58 +0700

    fix soap service

--- src/main/java/com/app2/engine/soap/CalculatorClient.java
+++ src/main/java/com/app2/engine/soap/CalculatorClient.java
@@ -4,10 +4,23 @@ import com.calculator.wsdl.Add;
 import com.calculator.wsdl.AddResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
+import org.springframework.ws.soap.saaj.SaajSoapMessage;
 
-public class CalculatorClient extends WebServiceGatewaySupport {
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
+public class CalculatorClient extends WebServiceGatewaySupport implements WebServiceMessageCallback {
+
+
 
     private static Logger LOGGER = LoggerFactory.getLogger(CalculatorClient.class);
 
@@ -15,7 +28,6 @@ public class CalculatorClient extends WebServiceGatewaySupport {
         Add request = new Add();
         request.setIntA(a);
         request.setIntB(b);
-
         AddResponse response = (AddResponse) getWebServiceTemplate()
                 .marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", request,
                         new SoapActionCallback("http://tempuri.org/Add"));
@@ -23,4 +35,18 @@ public class CalculatorClient extends WebServiceGatewaySupport {
         return response;
     }
 
+    public void doWithMessage(WebServiceMessage message) {
+        try {
+            SOAPMessage soapMessage = ((SaajSoapMessage)message).getSaajMessage();
+            SOAPHeader header = soapMessage.getSOAPHeader();
+            SOAPHeaderElement security = header.addHeaderElement(new QName("http://schemas.xmlsoap.org/ws/2003/06/secext", "Security", "wsse"));
+            SOAPElement usernameToken = security.addChildElement("UsernameToken", "wsse");
+            SOAPElement username = usernameToken.addChildElement("Username", "wsse");
+            SOAPElement password = usernameToken.addChildElement("Password", "wsse");
+            username.setTextContent("X");
+            password.setTextContent("Y");
+        } catch (Exception e) {
+            //... handle appropriately
+        }
+    }
 }
